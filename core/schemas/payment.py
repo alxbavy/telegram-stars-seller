ed2d50de2da3ff458a2cf904a34126d1a4c3d455
Artyom <sa.hei@mail.ru>
2026-03-27 05:28:41 +0300

    feat, refactor: add stars sending
    - Send stars in PaymentService
    - Add TransactionStatus StrEnum
    - Change transaction_repo argument to accept transaction object instead of transaction id. That creates less dependency
    - Can't pass transaction object to FragmentAPI, because FragmentAPI shouldn't know anything about transactions

--- core/schemas/payment.py
+++ core/schemas/payment.py
@@ -1,8 +1,16 @@
 from dataclasses import dataclass
 from decimal import Decimal
+from enum import StrEnum
+
 
 @dataclass(frozen=True)
 class PaymentDTO:
     transaction_id: int
     pay_url: str
     amount: Decimal
+
+
+class TransactionStatus(StrEnum):
+    PENDING = "PENDING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"

c067cc3255f7f5bc7491f62ab0adb2f05f799f46
Artyom <sa.hei@mail.ru>
2026-03-29 00:56:06 +0300

    feat: add sync_to_async to UserRepository method stubs
    - Add sync_to_async for use with "await" keyword

--- core/repositories/user_repository.py
+++ core/repositories/user_repository.py
@@ -1,15 +1,23 @@
+from asgiref.sync import sync_to_async
+
+
 class UserRepository:
+    @sync_to_async(thread_sensitive=True)
     def create_telegram_user(self, telegram_id, username):
         ...
 
+    @sync_to_async(thread_sensitive=True)
     def get_by_telegram_id(self, telegram_id):
         ...
 
+    @sync_to_async(thread_sensitive=True)
     def get_by_username(self, username):
         ...
 
+    @sync_to_async(thread_sensitive=True)
     def get_many_by_date_period(self):
         ...
 
+    @sync_to_async(thread_sensitive=True)
     def delete_user(self, user):
         ...

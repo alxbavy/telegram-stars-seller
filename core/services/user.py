243b2b4dc258502d2a031dc4e080d05edf352616
Artyom <sa.hei@mail.ru>
2026-03-30 00:06:39 +0300

    refactor: correct usage of user_repo in UserService
    - What to do if user is not registered?
    - Make class fields (repos) private

--- core/services/user.py
+++ core/services/user.py
@@ -1,18 +1,27 @@
-from core.repositories.user_repo import UserRepository
+from core.repositories.user_repository import UserRepository
 from core.repositories.trans_repo import TransactionRepository
 
 class UserService:
     def __init__(self, user_repo: UserRepository, trans_repo: TransactionRepository):
-        self.user_repo = user_repo
-        self.trans_repo = trans_repo
+        self._user_repo = user_repo
+        self._trans_repo = trans_repo
 
-    async def get_profile_data(self, user_id: int):
-        user = await self.user_repo.get_or_create(user_id)
-        stats = await self.trans_repo.get_user_stats(user_id)
-        return UserProfileDTO()
+    async def get_profile_data(self, user_id: int) -> UserProfileDTO | None:
+        user = await self._user_repo.get_by_telegram_id(user_id)
 
-        {
+        # TODO: порядок действий, если юзер не зарегистрирован? пока return None
+        if not user:
+            return None
+
+        stats = await self._trans_repo.get_user_stats(user)
+        return UserProfileDTO({
             "user": user,
             "total_stars": stats['total_stars'],
-            "orders_count": stats['orders_count']
-        }
+            "orders_count": stats['orders_count'],
+        })
+
+        # {
+        #     "user": user,
+        #     "total_stars": stats['total_stars'],
+        #     "orders_count": stats['orders_count']
+        # }

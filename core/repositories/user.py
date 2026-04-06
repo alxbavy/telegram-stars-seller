d62bce3f0b6ae5fd543612c12638462c97061a1b
Artyom <sa.hei@mail.ru>
2026-04-06 12:48:21 +0300

    refactor: change UserRepository to pure async instead of @sync_to_async, make staticmethods

--- core/repositories/user.py
+++ core/repositories/user.py
@@ -1,38 +1,36 @@
 from datetime import datetime
 
-from asgiref.sync import sync_to_async
-
 from core.models import TelegramUser
 
 
 class UserRepository:
-    @sync_to_async(thread_sensitive=True)
-    def create_telegram_user(self, telegram_id: int, username: str) -> TelegramUser:
+    @staticmethod
+    async def create_telegram_user(telegram_id: int, username: str) -> TelegramUser:
+        return await TelegramUser.objects.acreate(telegram_id=telegram_id, username=username)
+
+    @staticmethod
+    async def get_by_telegram_id(telegram_id: int) -> TelegramUser | None:
+        return await TelegramUser.objects.filter(telegram_id=telegram_id).afirst()
+
+    @staticmethod
+    async def get_by_username(username: str) -> TelegramUser | None:
         clean_username = username.lstrip("@")
-        return TelegramUser.objects.create(telegram_id=telegram_id, username=clean_username)
+        return await TelegramUser.objects.filter(username=clean_username).afirst()
 
-    @sync_to_async(thread_sensitive=True)
-    def get_by_telegram_id(self, telegram_id: int) -> TelegramUser | None:
-        return TelegramUser.objects.filter(telegram_id=telegram_id).first()
-
-    @sync_to_async(thread_sensitive=True)
-    def get_by_username(self, username: str) -> TelegramUser | None:
-        clean_username = username.lstrip("@")
-        return TelegramUser.objects.filter(username=clean_username).first()
-
-    @sync_to_async(thread_sensitive=True)
-    def get_many_by_date_period(self, start_date: datetime, end_date: datetime) -> list[TelegramUser]:
+    @staticmethod
+    async def get_many_by_date_period(start_date: datetime, end_date: datetime) -> list[TelegramUser]:
         """Возвращает список пользователей, зарегистрированных в указанный период (включительно)."""
-        qs = TelegramUser.objects.filter(created_at__range=(start_date, end_date))
-        return list(qs)
+        return [
+            user async for user in TelegramUser.objects.filter(created_at__range=(start_date, end_date))
+        ]
 
-    @sync_to_async(thread_sensitive=True)
-    def update_username(self, user: TelegramUser, new_username: str) -> TelegramUser:
+    @staticmethod
+    async def update_username(user: TelegramUser, new_username: str) -> TelegramUser:
         """Обновляет юзернейм существующего пользователя."""
         user.username = new_username.lstrip("@")
-        user.save(update_fields=["username"])
+        await user.asave(update_fields=["username"])
         return user
 
-    @sync_to_async(thread_sensitive=True)
-    def delete_user(self, user: TelegramUser) -> None:
-        user.delete()
+    @staticmethod
+    async def delete_user(user: TelegramUser) -> None:
+        await user.adelete()

94ff7535a1aed409fe0269eaff8352a5e0dc1913
Artyom <sa.hei@mail.ru>
2026-04-06 15:05:28 +0300

    feat, refactor: make PaymentRepository pure async
    - Add aget_solo for singleton models
    - aget_solo uses Django's aget_or_create thus no waiting or thread issues
    - No sync_to_async at all

--- core/repositories/payment.py
+++ core/repositories/payment.py
@@ -1,37 +1,38 @@
 from decimal import Decimal
 
-from asgiref.sync import sync_to_async
-
 from core.models import GlobalSettings, ExchangeRate, PaymentMethod
 from core.schemas.payment import PricingDTO
 
 
 class PaymentRepository:
-    @sync_to_async(thread_sensitive=True)
-    def get_pricing_data(self) -> PricingDTO:
+    @staticmethod
+    async def get_pricing_data() -> PricingDTO:
         """Объединение двух SQL-запросов в один поток для производительности."""
         return PricingDTO(
-            settings=GlobalSettings.get_solo(),
-            exchange_rate=ExchangeRate.get_solo()
+            settings=await GlobalSettings.aget_solo(),
+            exchange_rate=await ExchangeRate.aget_solo()
         )
 
-    @sync_to_async(thread_sensitive=True)
-    def get_payment_method_by_name(self, payment_method: str) -> PaymentMethod | None:
+    @staticmethod
+    async def get_payment_method_by_name(payment_method: str) -> PaymentMethod | None:
         """Если переданного метода нет, или он неактивен, то вернётся None."""
-        return PaymentMethod.objects.filter(name=payment_method, is_active=True).first()
+        return await PaymentMethod.objects.filter(name=payment_method, is_active=True).afirst()
 
-    @sync_to_async(thread_sensitive=True)
-    def get_all_active_methods(self) -> list[PaymentMethod]:
+    @staticmethod
+    async def get_all_active_methods() -> list[PaymentMethod]:
         """Возвращает активные методы оплаты для отображения в боте."""
-        return list(PaymentMethod.objects.filter(is_active=True))
+        return [
+            method async for method in PaymentMethod.objects.filter(is_active=True)
+        ]
 
-    @sync_to_async(thread_sensitive=True)
-    def update_current_usd_rate(self, current_usd_rate: Decimal) -> None:
+    @staticmethod
+    async def update_current_usd_rate(current_usd_rate: Decimal) -> None:
         """Метод для сохранения текущего курса доллара при его обновлении по таймеру."""
-        exchange_rate = ExchangeRate.get_solo()
+        exchange_rate = await ExchangeRate.aget_solo()
         exchange_rate.usd_rate = current_usd_rate
-        exchange_rate.save(update_fields=["usd_rate", "updated_at"])
+        await exchange_rate.asave(update_fields=["usd_rate", "updated_at"])
 
-    @sync_to_async(thread_sensitive=True)
-    def is_maintenance_mode(self) -> bool:
-        return GlobalSettings.get_solo().maintenance_mode
+    @staticmethod
+    async def is_maintenance_mode() -> bool:
+        settings = await GlobalSettings.aget_solo()
+        return settings.maintenance_mode

adf1ef62d7ed805c7280e990ef0394b605f858b9
Artyom <sa.hei@mail.ru>
2026-05-10 19:34:22 +0300

    refactor: add commented code for ignoring per_message=... warning
    - This warning happens always no matter which value to use
    - Details are written in TODO

--- core/management/commands/runbot.py
+++ core/management/commands/runbot.py
@@ -1,9 +1,11 @@
 import os
+import warnings
 
 from django.core.management.base import BaseCommand
 from dishka import make_async_container
 from telegram import Update
 from telegram.ext import ApplicationBuilder, TypeHandler
+from telegram.warnings import PTBUserWarning
 
 from bot.middlewares.user import register_user_middleware
 from core.ioc import BusinessLogicProvider
@@ -13,6 +15,15 @@ class Command(BaseCommand):
     help = "Запуск Telegram бота"
 
     def handle(self, *args, **options):
+        # TODO: раскомментировать при релизе (сейчас возникает предупреждение о per_message=False, но оно возникает в любом
+        #       случае, т. е. и при True; от него зависит поведение того, как выбирается состояние для ConversationHandler;
+        #       при False оно одно для всех сообщений; при True то сообщение, которое генерирует функция начала Conversation,
+        #       будет сохранено по его id, и конкретно для него будет изменяться состояние, т.е. можно будет иметь несколько
+        #       Conversation со своими состояниями; в нашем случае нельзя использовать per_message=True, т.к. иногда
+        #       начальное сообщение от Conversation необходимо удалить и сделать новое - в таком случае id не обновится
+        #       для хэндлера)
+        # warnings.filterwarnings("ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
+
         self.stdout.write("Бот запускается...")
 
         token = os.getenv("TELEGRAM_BOT_TOKEN")

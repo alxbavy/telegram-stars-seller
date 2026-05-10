0dab0aa9a31f58bb59c1a5fe389ee0fd3a6b05d7
Artyom <sa.hei@mail.ru>
2026-05-11 00:30:51 +0300

    refactor: add asyncio.sleep(5) in TelegramService.resolve_username(...)

--- core/services/telegram.py
+++ core/services/telegram.py
@@ -1,4 +1,8 @@
+import asyncio
+
+
 class TelegramService:
     async def resolve_username(self, username: str) -> bool:
         """Проверяет, существует ли пользователь в Telegram."""
+        await asyncio.sleep(5)
         return True

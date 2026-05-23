7f8c3c7b1d7eeb266a70305fdb38df9f013392fb
Artyom <sa.hei@mail.ru>
2026-05-24 02:06:18 +0300

    feat, refactor: add decimals where they should be in models, add uniqueness constraints, add expires_at to transaction, add paymentapi model
    - Now there must be an payment API first, then payment method is many-to-one relation
    - Payment method now has external_id for API's usage
    - Payment method's is_active=False by default now
    - Change admin respectively, also add some type hints, and order transactions by -created_at
    - Add some type hints to models

--- core/models.py
+++ core/models.py
@@ -1,25 +1,40 @@
+from collections.abc import Iterable
+from decimal import Decimal
+from typing import TypedDict, Unpack, override, TYPE_CHECKING
+
 from django.db import models
 from solo.models import SingletonModel
 
-import uuid
 from core.domain.enums import TransactionStatus, TransactionType
+
+
+class SaveKwargs(TypedDict, total=False):
+    force_insert: bool
+    force_update: bool
+    using: str | None
+    update_fields: Iterable[str] | None
 
 
 class TelegramUser(models.Model):
     objects = models.Manager()
+    if TYPE_CHECKING:
+        transactions: models.manager.RelatedManager["Transaction"]
 
     telegram_id = models.BigIntegerField(unique=True, verbose_name="Telegram ID")
-    username = models.CharField(max_length=255, verbose_name="Username")
+    username = models.CharField(max_length=255, blank=True, verbose_name="Username")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации")
 
-    def save(self, *args, **kwargs):
+    @override
+    def save(self, **kwargs: Unpack[SaveKwargs]):
         self.username = self.username.lstrip("@")
-        super().save(*args, **kwargs)
+        super().save(**kwargs)
 
-    async def asave(self, *args, **kwargs):
+    @override
+    async def asave(self, **kwargs: Unpack[SaveKwargs]):
         self.username = self.username.lstrip("@")
-        await super().asave(*args, **kwargs)
+        await super().asave(**kwargs)
 
+    @override
     def __str__(self):
         return f"{self.username or self.telegram_id}"
 
@@ -30,8 +45,10 @@ class TelegramUser(models.Model):
 
 class Transaction(models.Model):
     objects = models.Manager()
+    if TYPE_CHECKING:
+        metadata_info: "TransactionMetadata"
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False, verbose_name="ID платежа")
+    id = models.UUIDField(primary_key=True, verbose_name="ID платежа", help_text="Это ID из внешнего API")
     telegram_user = models.ForeignKey(
         TelegramUser,
         on_delete=models.CASCADE,
@@ -40,32 +57,26 @@ class Transaction(models.Model):
     )
     amount_fiat = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма")
     amount_stars = models.IntegerField(verbose_name="Количество звезд")
-    target_username = models.CharField(max_length=255, null=True, blank=True, verbose_name="Кому")
+    target_username = models.CharField(max_length=255, blank=True, default="Себе", verbose_name="Кому")
     status = models.CharField(max_length=20, choices=TransactionStatus.to_choices(), default=TransactionStatus.PENDING, verbose_name="Статус")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    expires_at = models.DateTimeField(null=True, blank=True, verbose_name="Истекает")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Последнее обновление")
 
-    def _set_target_username_if_none(self) -> None:
-        """
-        Если при сохранении target_username пустой (не указан подарок), то target_username = self.telegram_user.username
-        """
-        if not self.target_username:
-            self.target_username = self.telegram_user.username
-
-    def save(self, *args, **kwargs) -> None:
-        self._set_target_username_if_none()
-        super().save(*args, **kwargs)
-
-    async def asave(self, *args, **kwargs) -> None:
-        self._set_target_username_if_none()
-        await super().asave(*args, **kwargs)
-
+    @override
     def __str__(self):
         return f"Транзакция #{self.id} ({self.telegram_user})"
 
     class Meta:
         verbose_name = "Транзакция"
         verbose_name_plural = "Транзакции"
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["id", "telegram_user"],
+                name="unique_user_transaction_id"
+            )
+        ]
 
 
 class TransactionMetadata(models.Model):
@@ -79,7 +90,11 @@ class TransactionMetadata(models.Model):
     )
     type = models.CharField(max_length=50, choices=TransactionType.to_choices(), verbose_name="Тип")
     payment_method = models.CharField(max_length=50, verbose_name="Способ оплаты")
-    payload = models.JSONField(default=dict, blank=True, verbose_name="Доп. данные (JSON)")
+    payload: dict[str, object] = models.JSONField(default=dict, blank=True, verbose_name="Доп. данные (JSON)")
+
+    @override
+    def __str__(self):
+        return f"Метаданные для {self.transaction}"
 
     class Meta:
         verbose_name = "Метаданные транзакции"
@@ -93,24 +108,55 @@ class MonthlyProfit(Transaction):
         verbose_name_plural = "Прибыль по месяцам"
 
 
+class PaymentAPI(models.Model):
+    objects = models.Manager()
+    if TYPE_CHECKING:
+        methods: models.manager.RelatedManager["PaymentMethod"]
+
+    name = models.CharField(primary_key=True, max_length=50, verbose_name="Название API платёжных систем")
+
+    @override
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "API платёжных систем"
+        verbose_name_plural = "API платёжных систем"
+
+
 class PaymentMethod(models.Model):
     objects = models.Manager()
 
-    name = models.CharField(max_length=100, verbose_name="Название системы")
+    api = models.ForeignKey(
+        PaymentAPI,
+        on_delete=models.CASCADE,
+        related_name="methods",
+        verbose_name="API платёжных систем"
+    )
+    name = models.CharField(max_length=50, verbose_name="Название метода оплаты", help_text="Отображается в боте")
+    external_id = models.CharField(max_length=255, verbose_name="ID метода оплаты", help_text="ID из внешнего API; Может быть числом или строкой")
     commission_percent = models.DecimalField(
         max_digits=5,
         decimal_places=2,
-        default=0.0,
+        default=Decimal("0.00"),
         verbose_name="Комиссия (%)"
     )
-    is_active = models.BooleanField(default=True, verbose_name="Активна")
+    is_active = models.BooleanField(default=False, verbose_name="Активна")
 
+    @override
     def __str__(self):
-        return f"{self.name} ({self.commission_percent}%)"
+        return f"{self.api.name} - {self.name} ({self.commission_percent}%)"
 
     class Meta:
         verbose_name = "Метод оплаты"
         verbose_name_plural = "Методы оплаты"
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["api", "name"],
+                name="unique_api_method_name"
+            )
+        ]
 
 
 class GlobalSettings(SingletonModel):
@@ -119,13 +165,13 @@ class GlobalSettings(SingletonModel):
     star_base_cost = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=1.35,
+        default=Decimal("1.35"),
         verbose_name="Базовая цена одной звезды"
     )
     usd_base_rate = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=80.0,
+        default=Decimal("80.00"),
         verbose_name="Базовый курс USD"
     )
     is_use_usd_rate = models.BooleanField(
@@ -155,7 +201,7 @@ class ExchangeRate(SingletonModel):
     usd_rate = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=90.0,
+        default=Decimal("90.00"),
         verbose_name="Текущий курс USD"
     )
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Последнее обновление")
@@ -165,6 +211,7 @@ class ExchangeRate(SingletonModel):
         obj, is_created = await cls.objects.aget_or_create(pk=cls.singleton_instance_id)
         return obj
 
+    @override
     def __str__(self):
         return f"Курс доллара: {self.usd_rate}"
 
@@ -172,6 +219,7 @@ class ExchangeRate(SingletonModel):
         verbose_name = "Курс валют"
 
 
+# TODO: Вроде бы не надо, так как для Persistence будет PicklePersistence
 class BotState(models.Model):
     objects = models.Manager()
 

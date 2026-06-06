d41090bb7d9b373697cd25167d0f958de013e362
Artyom <sa.hei@mail.ru>
2026-06-06 22:22:53 +0300

    chore, build, fix: activate WAL for sqlite for async support, use asgi for async
    - Make STATIC_URL = "/static/" instead of "static/" - 2nd variant is relative which may broke static content, and 1st is absolute

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,19 @@
+from collections.abc import Mapping
+
 from django.apps import AppConfig
+from django.dispatch import receiver
+from django.db.backends.base.base import BaseDatabaseWrapper
+from django.db.backends.signals import connection_created
 
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+
+@receiver(connection_created)
+def configure_sqlite(sender: object, connection: BaseDatabaseWrapper, **kwargs: Mapping[str, object]):
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA journal_mode = WAL;")    # Для параллельного чтения
+        cursor.execute("PRAGMA busy_timeout = 10000;")  # При одновременной записи будет ожидание, указывается в мс
+        cursor.execute("PRAGMA synchronous = NORMAL;")  # Синхронизация с диском

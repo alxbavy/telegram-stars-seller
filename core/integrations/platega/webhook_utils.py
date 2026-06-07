3d0aa18c770d8b52f1e166781591688fcde48b26
Artyom <sa.hei@mail.ru>
2026-06-08 00:18:49 +0300

    refactor, feat, fix: move all payment logic from webhook to celery shared_task, add lock on concrete transaction_id
    - If transaction status is SUCCESS at the start of the task then do nothing
    - Instead of deleting order message remove reply markup, in order to save transaction ID at least somewhere for worst case scenario
    - Add "async with bot" for proper connection closing because bot uses aiohttp sessions

--- core/integrations/platega/webhook_utils.py
+++ core/integrations/platega/webhook_utils.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from typing import cast
+from uuid import UUID
 
 from telegram.ext import ExtBot
 
@@ -76,78 +77,95 @@ async def get_support_url() -> str:
         return await support_service.get_support_url()
 
 
-async def safe_delete_order_message(bot: ExtBot[None], user_id: int, message_id: int) -> None:
+async def safe_remove_reply_markup_for_order_message(bot: ExtBot[None], user_id: int, message_id: int) -> None:
     try:
-        _ = await bot.delete_message(chat_id=user_id, message_id=message_id)
+        _ = await bot.edit_message_reply_markup(chat_id=user_id, message_id=message_id, reply_markup=None)
     except Exception as err:
-        logger.exception(f"Error while trying to delete message: {user_id = }, {message_id = }\n{err = }")
-        pass
+        log_msg = (
+            f"Error while trying to remove reply_markup for order message: {user_id = }, {message_id = }\n{err = }"
+        )
+        logger.exception(log_msg)
 
 
 async def safe_process_transaction(
-        data: PlategaRequestJson,
-        bot: ExtBot[None], parse_mode: str, user_id: int, support_url: str
-) -> tuple[bool, Transaction | None]:
-    transaction_id = data.get("id")
+        data: PlategaRequestJson, parsed_payload: PaymentPayloadDict | None
+) -> str | Transaction | TransactionStatus | None:
+    """
+    - Если error_msg не None, значит произошла ошибка, тогда второй объект будет None.
+
+    - Перед обработкой будет попытка получить транзакцию - если не получится, будет возвращена ошибка и None.
+
+    - Если транзакция была получена, и она уже SUCCESS, тогда будет возвращено None и None.
+
+    - Далее, если при обработке транзакции ошибки не было, то второй объект будет либо транзакцией, либо
+    итоговым статусом на случай проблем с БД.
+    """
+
+    try:
+        transaction_uuid = UUID(data.get("id"))
+    except Exception as uuid_err:
+        logger.exception("Couldn't convert transaction id to UUID")
+        return f"{uuid_err.__class__.__name__}: {uuid_err}"
+
     try:
         async with container() as request_container:
             payment_service = await request_container.get(PaymentService)
+
+            transaction = await payment_service.get_transaction_by_uuid(
+                transaction_uuid,
+                data, parsed_payload
+            )
+            if transaction is None:
+                return f"Couldn't get or create transaction {transaction_uuid}"
+
+            if transaction.status == TransactionStatus.SUCCESS:
+                return None
+
             if data["status"] != "CONFIRMED":
-                transaction = await payment_service.cancel_transaction(transaction_id, data)
+                transaction = await payment_service.cancel_transaction(transaction, data)
             else:
-                transaction = await payment_service.confirm_payment(transaction_id)
+                transaction = await payment_service.confirm_payment(transaction)
+
+            return transaction
 
     except Exception as transaction_err:
-        text = (
-            f"❌ <b>Произошла ошибка!</b>\n\n"
-            f"Обратись в тех. поддержку с ID заказа и текстом ошибки или можешь сделать новый заказ с помощью /start\n"
-            f"🆔 ID заказа: <code>{transaction_id}</code>\n\n"
-            f"Текст ошибки:\n<pre>{transaction_err.__class__.__name__}: {transaction_err}</pre>"
-        )
-        try:
-            _ = await bot.send_message(
-                chat_id=user_id, text=text,
-                reply_markup=build_error_kb(support_url), parse_mode=parse_mode
-            )
-        except Exception as err:
-            logger.exception(f"Error while sending error message in bot:\n{err = }")
-
         logger.exception(f"Error during transaction processing:\n{transaction_err = }")
-        return False, None
-
-    return True, transaction
+        return f"{transaction_err.__class__.__name__}: {transaction_err}"
 
 
-async def create_missing_transaction(data: PlategaRequestJson, parsed_payload: PaymentPayloadDict) -> Transaction:
-    async with container() as request_container:
-        payment_service = await request_container.get(PaymentService)
-        return await payment_service.create_transaction(
-            data["id"], parsed_payload["user_id"],
-            data["amount"], parsed_payload["stars_count"],
-            "Platega (Generated)", str(data["paymentMethod"]),
-            parsed_payload.get("target_username", ""),
-            TransactionStatus.CANCELLED if data["status"] != "CONFIRMED" else TransactionStatus.SUCCESS,
-            data
-        )
+async def safe_notify_user(
+        bot: ExtBot[None], parse_mode: str,
+        transaction: Transaction | TransactionStatus, transaction_id: str,
+        user_id: int, support_url: str
+) -> None:
+    if isinstance(transaction, Transaction) and transaction.status == TransactionStatus.SUCCESS:
+            text = (
+                f"😊 <b>Заказ успешно доставлен!</b>\n\n"
+                f"Пополнили — ⭐ {transaction.amount_stars} звёзд\n"
+                f'{"Для кого — @" + transaction.target_username + "\n" if transaction.target_username != "Себе" else ""}\n'
+                f"Спасибо за покупку! ❤️\n\n"
+                f"✨ <b>Сделать ещё заказ — /start</b>"
+            )
+            reply_markup = None
 
-
-async def safe_notify_user(bot: ExtBot[None], parse_mode: str, transaction: Transaction, support_url: str) -> None:
-    if transaction.status == TransactionStatus.SUCCESS:
+    elif isinstance(transaction, TransactionStatus) and transaction == TransactionStatus.SUCCESS:
         text = (
-            f"😊 <b>Заказ успешно доставлен!</b>\n\n"
-            f"Пополнили — ⭐ {transaction.amount_stars} звёзд\n"
-            f'{"Для кого — @" + transaction.target_username + "\n" if transaction.target_username != "Себе" else ""}\n'
-            f"Спасибо за покупку! ❤️\n\n"
+            f"🤕 <b>При обработке заказа возникли трудности...</b>\n\n"
+            f"Однако! Звёзды должны быть отправлены 🙏\n"
+            f"Если ничего не придёт в течение 5 минут, обратись в тех. поддержку. Извини за неудобства! ❤️\n\n"
             f"✨ <b>Сделать ещё заказ — /start</b>"
         )
-        reply_markup = None
+        reply_markup = build_error_kb(support_url)
 
-    elif transaction.status == TransactionStatus.CANCELLED:
+    elif (
+            isinstance(transaction, Transaction) and transaction.status == TransactionStatus.CANCELLED
+            or transaction == TransactionStatus.CANCELLED
+    ):
         text = (
             f"⚠️ <b>Твоему заказу был присвоен статус {TransactionStatus.CANCELLED.translation}</b>\n\n"
             f"Если ты решил не делать заказ, то можешь проигнорировать это сообщение\n\n"
             f"В ином случае обратись в тех. поддержку с ID заказа\n"
-            f"🆔 ID заказа: <code>{transaction.id}</code>"
+            f"🆔 ID заказа: <code>{transaction_id}</code>"
         )
         reply_markup = build_error_kb(support_url)
 
@@ -155,17 +173,19 @@ async def safe_notify_user(bot: ExtBot[None], parse_mode: str, transaction: Tran
         text = (
             f"❌ <b>Произошла ошибка при переводе звёзд!</b>\n\n"
             f"Обратись в тех. поддержку с ID заказа и текстом ошибки\n\n"
-            f"🆔 ID заказа: <code>{transaction.id}</code>\n"
-            f"Текст ошибки:\n<pre>{json.dumps(transaction.metadata_info.payload, indent=2)}</pre>"
+            f"🆔 ID заказа: <code>{transaction_id}</code>\n"
         )
+        if isinstance(transaction, Transaction):
+            text += f"Текст ошибки:\n<pre>{json.dumps(transaction.metadata_info.payload, indent=2)}</pre>"
+        else:
+            text += "Текст ошибки:\n<pre>Произошла ошибка БД, поэтому полный текст ошибки доступен в логах</pre>"
         reply_markup = build_error_kb(support_url)
 
     try:
         _ = await bot.send_message(
-            chat_id=transaction.telegram_user.telegram_id,
+            chat_id=user_id,
             text=text,
             reply_markup=reply_markup, parse_mode=parse_mode
         )
     except Exception as err:
         logger.exception(f"Error while sending message in bot:\n{err = }")
-        pass

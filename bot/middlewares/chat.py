44be853a76818bf88ba602c99f48025384f1220c
Artyom <sa.hei@mail.ru>
2026-07-30 23:33:28 +0300

    refactor, fix: organize variables in chat checker handler; ignore lamestars channel updates

--- bot/middlewares/chat.py
+++ bot/middlewares/chat.py
@@ -1,7 +1,6 @@
 import logging
 
-from telegram import Update
-from telegram.constants import ChatType, ChatMemberStatus
+from telegram import ChatMember, Update
 from telegram.ext import ApplicationHandlerStop, ContextTypes
 
 from django.conf import settings
@@ -10,7 +9,7 @@ from django.conf import settings
 logger = logging.getLogger(__name__)
 
 
-async def enforce_private_chats_only_or_admin_chat(update: object, _: ContextTypes.DEFAULT_TYPE):
+async def enforce_private_chats_only_or_admin_chat(update: object, _: ContextTypes.DEFAULT_TYPE) -> None:
     if not isinstance(update, Update):
         return
 
@@ -19,24 +18,26 @@ async def enforce_private_chats_only_or_admin_chat(update: object, _: ContextTyp
     if not chat:
         return
 
-    if chat.type == ChatType.PRIVATE:
+    if chat.type == chat.PRIVATE:
         return
 
     if update.my_chat_member:
-        status = update.my_chat_member.new_chat_member.status
-        if status in [ChatMemberStatus.LEFT, ChatMemberStatus.BANNED]:
+        chat_member = update.my_chat_member.new_chat_member
+        status = chat_member.status
+        if status in [chat_member.LEFT, chat_member.BANNED]:
             raise ApplicationHandlerStop()
 
-    if chat.id != settings.ADMIN_CHAT_ID:
+    chat_id = chat.id
+    if chat_id != settings.ADMIN_CHAT_ID and chat_id != settings.CHANNEL_ID:  # pyright: ignore[reportAny]
         try:
-            logger.info(f"Обнаружен несанкционированный чат {chat.title} ({chat.id}). Попытка выхода...")
+            logger.info(f"Обнаружен несанкционированный чат {chat.title} ({chat_id}). Попытка выхода...")
             if await chat.leave():
                 logger.info("Выход из чата прошёл успешно!")
             else:
-                logger.error(f"Не удалось выйти из чата {chat.id} по неизвестной причине (возможно сбой сети)")
+                logger.error(f"Не удалось выйти из чата {chat_id} по неизвестной причине (возможно сбой сети)")
 
         except Exception as exc:
             if "not a member" not in str(exc):
-                logger.error(f"Не удалось выйти из чата {chat.id}: {exc}")
+                logger.error(f"Не удалось выйти из чата {chat_id}: {exc}")
 
     raise ApplicationHandlerStop()
